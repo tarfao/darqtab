78dfc535e8ea4baa5037b01e5d39a961bc0e85c7
tarfao <tarfao@hotmail.com>
2025-01-26 19:06:06 -0300

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br/ para o https://curso.dev
+
+//fundação
+nodejs
+
+rc = run commands
+
+// paredes
+
+next.js 13
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Love you, xuxu!</h1>
+  return <h1>Love you, xuxu!</h1>;
+}
+
+function teste() {
+  console.log("alouuu");
 }
 
 export default Home;
