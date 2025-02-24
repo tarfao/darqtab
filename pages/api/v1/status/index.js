64ddcfd2fb51740f247287b96c67790f46de086c
tarfao <tarfao@hotmail.com>
2025-02-24 20:50:24 -0300

    build: add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   console.log((await database.query("SELECT 1 + 1;")).rows);
