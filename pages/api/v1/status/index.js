476a65486c1c462cc3ec23d8b4bd98839ba95892
tarfao <tarfao@hotmail.com>
2025-03-11 23:18:43 -0300

    feat: finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  console.log((await database.query("SELECT 1 + 1;")).rows);
+  const updatedAt = new Date();
+  const databaseVersion = (await database.query("SHOW server_version;")).rows;
+  const databaseVersionValue = databaseVersion[0].server_version;
 
-  response
-    .status(200)
-    .json({ key: "Fala galera, é hoje que só acaba amanhã." });
+  const [{ max_connections: maxConnections }] = (
+    await database.query("SHOW max_connections;")
+  ).rows;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const connections = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnections = connections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      version: databaseVersionValue,
+      max_connections: parseInt(maxConnections),
+      used_connections: openedConnections,
+    },
+  });
 }
 
 export default status;
