11fa4b19c53bcdace4e3d9b70dfcdf9fdec2cf43
Ronaldo Souza <ronaldosouza@MacBook-Air-de-Ronaldo.local>
2025-04-08 19:23:21 -0400

    add more robust ssl configuration

--- infra/database.js
+++ infra/database.js
@@ -7,16 +7,7 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-
-  console.log("Credenciais do Postgres: ", {
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -34,3 +25,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if(process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA
+    }
+  }
+
+  return process.env.NODE_ENV === 'development' ? false : true;
+}
