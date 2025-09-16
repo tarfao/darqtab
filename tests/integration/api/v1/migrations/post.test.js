d661bd5817200a014eab5deb437ffcff8e96001d
Ronaldo Tafarel <ronaldo.souza@dbserver.com.br>
2025-09-15 22:01:03 -0400

    chore: fix lint

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,7 +6,7 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET to /api/v1/status should return 200", async () => {
+test("GET to /api/v1/status should return 201", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
