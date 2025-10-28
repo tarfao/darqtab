d78a5dc0ab8a366d509b079e4db789cb7d934d9c
Ronaldo Tafarel <ronaldo.souza@dbserver.com.br>
2025-10-27 22:14:41 -0400

    feat: create and use internal server error in api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,30 +1,39 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(request, response) {
-  const updatedAt = new Date();
-  const databaseVersion = (await database.query("SHOW server_version;")).rows;
-  const databaseVersionValue = databaseVersion[0].server_version;
+  try {
+    const updatedAt = new Date();
+    const databaseVersion = (await database.query("SHOW server_version;")).rows;
+    const databaseVersionValue = databaseVersion[0].server_version;
 
-  const [{ max_connections: maxConnections }] = (
-    await database.query("SHOW max_connections;")
-  ).rows;
+    const [{ max_connections: maxConnections }] = (
+      await database.query("SHOW max_connections;")
+    ).rows;
 
-  const databaseName = process.env.POSTGRES_DB;
+    const databaseName = process.env.POSTGRES_DB;
 
-  const connections = await database.query({
-    text: "SELECT count(*) FROM pg_stat_activity where datname = $1;",
-    values: [databaseName],
-  });
-  const openedConnections = connections.rows[0].count;
+    const connections = await database.query({
+      text: "SELECT count(*) FROM pg_stat_activity where datname = $1;",
+      values: [databaseName],
+    });
+    const openedConnections = connections.rows[0].count;
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      version: databaseVersionValue,
-      max_connections: parseInt(maxConnections),
-      used_connections: openedConnections,
-    },
-  });
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        version: databaseVersionValue,
+        max_connections: parseInt(maxConnections),
+        used_connections: openedConnections,
+      },
+    });
+  } catch (error) {
+    const publicError = new InternalServerError({
+      cause: error,
+    });
+
+    response.status(500).json(publicError);
+  }
 }
 
 export default status;
